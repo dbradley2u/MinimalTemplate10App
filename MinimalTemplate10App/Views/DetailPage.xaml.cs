44666d23c0a86bc6ec5719b21e869473bcd6df04
Denise <Denise@DENISE-PC>
2016-05-17 22:42:39 -0400

    Add project files.

---
+++ MinimalTemplate10App/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using MinimalTemplate10App.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace MinimalTemplate10App.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
